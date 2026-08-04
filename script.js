1b8dec59257069c94916a9af6eac98cb05c38b98
aayushi1403 <aayushir685@gmail.com>
2026-08-04 23:46:31 +0530

    making expense tracker

---
+++ script.js
@@ -0,0 +1,9 @@
+let balance=document.querySelector("#balance")
+let income=document.querySelector("#money-plus")
+let expense=document.querySelector("#money-minus")
+let userList=document.querySelector("#list")
+let cross=document.querySelector(".delete-btn")
+
+let newTransactionDetail=document.querySelector("#text")
+let newAmount=document.querySelector("#amountSpent")
+let addTransaction=document.querySelector("#btn")
