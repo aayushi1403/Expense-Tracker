28c9297d828ebcd9d712302106f242cfce5dd5aa
aayushi1403 <aayushir685@gmail.com>
2026-08-17 02:25:37 +0530

    data type identified

--- script.js
+++ script.js
@@ -2,10 +2,22 @@ let balance=document.querySelector("#balance")
 let income=document.querySelector("#money-plus")
 let expense=document.querySelector("#money-minus")
 let userList=document.querySelector("#list")
-let cross=document.querySelector(".delete-btn")
 
 let newTransactionDetail=document.querySelector("#text")
-let newAmount=document.querySelector("#amountSpent")
-let addTransaction=document.querySelector("#btn")
+let newAmount=document.querySelector("#amount")
+let addTransaction=document.querySelector(".btn")
+let form = document.querySelector("#form");
+let TransactionObj=
+{
+  description:"",
+  amount:0
+}
+let TransactionArray=[]
 
-let transactionList=document.querySelector(".list")
+form.addEventListener('submit',(e)=>{
+  e.preventDefault();
+  let des=newTransactionDetail.value;
+  let userAmount=newAmount.value;
+  console.log(des)
+  console.log(userAmount)
+})
