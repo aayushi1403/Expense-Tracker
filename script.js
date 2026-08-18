0388bba6e645eff7084261efdfaa5fdce8541e6a
aayushi1403 <aayushir685@gmail.com>
2026-08-19 02:56:50 +0530

    adding elements and deleting

--- script.js
+++ script.js
@@ -13,10 +13,39 @@ let TransactionObj=
   amount:0
 }
 let TransactionArray=[]
-
+let index=0;
 form.addEventListener('submit',(e)=>{
   e.preventDefault();
   let des=newTransactionDetail.value;
   let userAmount=newAmount.value;
-  
+  let transaction={
+    id:index,
+    description:des,
+    amount:Number(userAmount)
+  }
+  index++;
+    
+    TransactionArray=[...TransactionArray,transaction]
+    displayTransaction();
 })
+
+function displayTransaction() {
+  TransactionArray.forEach((element) => {
+    userList.insertAdjacentHTML(
+      "beforeend",
+      `<li>
+        ${element.description} ${element.amount}
+        <button class="deleteButton" data-id=${element.id}>
+          <i class="fa fa-trash"></i>
+        </button>
+      </li>`
+    );
+  });
+}
+userList.addEventListener('click',(e)=>{deleting(e.target.closest('.deleteButton').dataset.id,e.target.closest('li'))})
+function deleting(id,item){
+  const temp=TransactionArray.filter((element)=>element.id!=Number(id))
+  TransactionArray=temp;
+
+  item.remove();
+}
